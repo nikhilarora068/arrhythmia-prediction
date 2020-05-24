13bac5e9263a8fb453b3409caa8f406d8729e619
divya <divyansh.srivastavadps@gmail.com>
2020-05-24 23:35:21 +0530

    Changes: All back-end changes completed

--- prediction/views.py
+++ prediction/views.py
@@ -7,8 +7,8 @@ import tensorflow
 from tensorflow.keras.preprocessing.image import ImageDataGenerator, img_to_array, load_img
 import numpy as np
 
-# model = tensorflow.keras.models.load_model('/home/Linux/Predict_types_of_arrhythmia.h5')
-model = tensorflow.keras.models.load_model('/Users/NikhilArora/desktop/Major-II/breakthrough/model3.h5')
+model = tensorflow.keras.models.load_model('/home/Linux/Predict_types_of_arrhythmia.h5')
+# model = tensorflow.keras.models.load_model('C:\\Users\\divya\\Desktop\\ECG_TO_IMAGE\\Trained Models\\Predict_types_of_arrhythmia.h5')
 print('loaded model')
 
 
@@ -23,37 +23,31 @@ def home(request):
         mime = mime + ";" if mime else ";"
         input_image = "data:%sbase64,%s" % (mime, encoded)
 
-        image = load_img(request.FILES['myfile'], target_size=(128, 128))
+        image = load_img(request.FILES['myfile'], target_size=(224, 224))
 
         image = img_to_array(image)
         image = np.expand_dims(image, axis=0)
         image /= 255.
 
-        pred = model.predict(image.reshape((1, 128, 128, 3)))
-        pred_class = pred.argmax(axis=-1)
+        pred = model.predict(image.reshape((1, 224, 224, 3)))
+        pred_class = pred.argmax(axis=1)
 
         # folder order 'L' 'N' 'P' 'R' 'V'
 
-        L = pred[0][1]
-        N = pred[0][2]
-        P = pred[0][3]
-        R = pred[0][4]
-        V = pred[0][5]
-
         type = ""
         status = 0
-        probability = max(L, N, P, R, V)
+        probability = str(pred[0][pred_class]).strip('[]')
 
-        if probability == N:
+        if pred_class == 1:
+            type = 'Left bundle branch block beat'
+        elif pred_class == 2:
             type = 'Normal heartbeat'
             status = 1
-        elif probability == L:
-            type = 'Left bundle branch block beat'
-        elif probability == P:
+        elif pred_class == 3:
             type = 'Paced beat'
-        elif probability == R:
+        elif pred_class == 4:
             type = 'Right bundle branch block beat'
-        elif probability == V:
+        elif pred_class == 5:
             type = 'Premature ventricular contraction'
 
         print("probability = ", probability)
@@ -61,7 +55,7 @@ def home(request):
         context = {
             'image': input_image,
             'status': status,
-            'accuracy': probability,
+            'accuracy': float(probability)*100,
             'type': type
         }
 
